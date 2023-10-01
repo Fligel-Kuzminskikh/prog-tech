92e5d4be89e710d5f120e9b6810a481e22e5268c
@romanlisiukov <romanlisyukov@gmail.com>
2023-10-01 23:28:14 +0300

    Util that creates database of hard drive's files

---
+++ scripts/create_collection.py
@@ -0,0 +1,28 @@
+import sys
+
+sys.path = ["C:\\Users\\User\\prog-tech"] + sys.path
+
+from staff import collector
+
+# Initializes collector object
+c = collector.Collector()
+
+
+# Collects data on files in hard drive
+def collect_files_data():
+    c.get_list_files_names_paths()
+    c.get_list_sizes()
+    c.get_list_dates_created()
+    c.get_list_dates_last_modified()
+
+
+# Initiates create_collection function
+def create_collection():
+    collect_files_data()
+    # Creates files.csv after data are collected
+    c.create_files_database()
+
+
+# Executes create_collection
+if __name__ == '__create_collection__':
+    create_collection()
