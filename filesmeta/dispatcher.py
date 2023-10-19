bc44b8ad2ed222f6360542ddddf9d259490c7caf
@romanlisiukov <romanlisyukov@gmail.com>
2023-10-19 19:23:17 +0300

    a class for extracting metadata on MS Office files (.pptx, .docx, .xl[st][tm]) and dispatcher that controls all workers

--- filesmeta/dispatcher.py
+++ filesmeta/dispatcher.py
@@ -1,4 +1,4 @@
-from filesmeta import worker_all, worker_images, worker_pdf
+from filesmeta import worker_all, worker_images, worker_pdf, worker_ms_office
 
 
 class Dispatcher:
@@ -6,3 +6,4 @@ class Dispatcher:
     worker_all = worker_all.WorkerAll()
     worker_images = worker_images.WorkerImages()
     worker_pdf = worker_pdf.WorkerPDF()
+    worker_ms_office = worker_ms_office.WorkerMsOffice()
---
+++ filesmeta/worker_ms_office.py
@@ -0,0 +1,54 @@
+import pandas as pd
+from docx import Document
+from pptx import Presentation
+from openpyxl import load_workbook
+
+
+class WorkerMsOffice:
+    docx_paths = list()
+    xl_paths = list()
+    pptx_paths = list()
+
+    def get_metadata_docx(self):
+        perm = pd.DataFrame()
+        for docx_path in self.docx_paths:
+            docx = Document(docx=docx_path)
+            properties = docx.core_properties
+            metadata = {}
+            for directory in dir(properties):
+                if not directory.startswith('_'):
+                    metadata[directory] = str(getattr(properties, directory))
+            temp = pd.DataFrame(metadata, index=[0])
+            temp["filepath"] = docx_path
+            perm = pd.concat([perm, temp])
+            return perm
+
+    def get_metadata_xl(self):
+        perm = pd.DataFrame()
+        for xl_path in self.xl_paths:
+            workbook = load_workbook(xl_path)
+            metadata = {}
+            for directory in dir(workbook.properties):
+                if not directory.startswith('_') and directory not in ["to_tree", "from_tree"]:
+                    metadata[directory] = str(getattr(workbook.properties, directory))
+            temp = pd.DataFrame(metadata, index=[0])
+            temp["filepath"] = xl_path
+            perm = pd.concat([perm, temp])
+            return perm
+
+    def get_metadata_pptx(self):
+        perm = pd.DataFrame()
+        for pptx_path in self.pptx_paths:
+            pptx = Presentation(pptx=pptx_path)
+            properties = pptx.core_properties
+            metadata = {}
+            for directory in dir(properties):
+                if not directory.startswith('_'):
+                    metadata[directory] = str(getattr(properties, directory))
+            temp = pd.DataFrame(metadata, index=[0])
+            temp["filepath"] = pptx_path
+            perm = pd.concat([perm, temp])
+            return perm
+
+    def get_metadata_ms_office(self):
+        return pd.concat([self.get_metadata_xl(), self.get_metadata_docx(), self.get_metadata_pptx()])
